0f143c94ab68002137d493518a438d1667660fc6
felixcruces <felixcruces94@gmail.com>
2018-06-28 16:27:43 +0200

    tags/titolo Search.php e piccolo modifica nel see_posts per poter testare i progressi fatti

--- ProgettoSaw/db/UserDeploy/seed_table_Posts.php
+++ ProgettoSaw/db/UserDeploy/seed_table_Posts.php
@@ -8,19 +8,19 @@
                 `Descrizione`, `Strumentazione Richiesta`, `Coordinata Nord`, `Coordinata Sud`,
                 `Citta`, `Username Autore`,`Tag`) VALUES
     
-    ('luna','Pesca immersione','8','2018-06-15','2018-06-16','marina di sestri',
+    ('luna','Pesca immersione la bella vita','8','2018-06-15','2018-06-16','marina di sestri',
     '180','piccola gita per pescare','boccaglio,pinne','45.60','36.88','Genova','zio','pesca, immersione'),
     
-    ('sirena tiger','paesaggio vela','5','2018-07-01','2018-07-06','fiera di genova',
+    ('sirena tiger','paesaggio vela la grande botta','5','2018-07-01','2018-07-06','fiera di genova',
     '250','viaggio in mezzo al mare','solo la voglia di ammirare il mare','20.156','26.895','Genova','kaio','paesaggio, vela'),
     
-    ('caronte express','vela immersione','5','2018-07-01','2018-07-06','genova est',
+    ('caronte express','vela immersione settima destinazione','5','2018-07-01','2018-07-06','genova est',
     '250','vela mania!!','niente','20.156','26.895','Genova','kaio','vela, immersione'),
 
-    ('traghetto','pesca immersione','5','2018-07-01','2018-07-06','genova est',
+    ('traghetto','pesca immersione franchino e ricky le roy','5','2018-07-01','2018-07-06','genova est',
     '250','vela mania!!','niente','20.156','26.895','Genova','kaio','barca solo a motore, immersione'),
 
-    ('traghetto','pesca vela','5','2018-07-01','2018-07-06','genova est',
+    ('traghetto','pesca vela mondo visione','5','2018-07-01','2018-07-06','genova est',
     '250','vela mania!!','niente','20.156','26.895','Genova','zio','pesca, vela')
         
         ;";
